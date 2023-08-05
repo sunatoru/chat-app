370d3acb32a96e12393e0907cbfa26a29c9b613a
tano <supright34@gmail.com>
2023-08-05 15:48:55 +0900

    Revert "messagesテーブルの作成"
    This reverts commit 39c9084b06fa0803e13ab1d6d4fa713a5b7bb5b4.

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_05_063609) do
-  create_table "messages", charset: "utf8", force: :cascade do |t|
-    t.string "content"
-    t.bigint "room_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["room_id"], name: "index_messages_on_room_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_150830) do
   create_table "room_users", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "room_id", null: false
@@ -49,8 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_05_063609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "messages", "rooms"
-  add_foreign_key "messages", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
 end
